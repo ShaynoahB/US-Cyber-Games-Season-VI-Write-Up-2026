72626302f97e9445dbf71a1271de62ccb6399fe4
ShaynoahB <103382420+ShaynoahB@users.noreply.github.com>
2026-06-13 19:30:26 -0400

    Create README.d

---
+++ Beginner Room Challenges/Forensics/Cyber Squirrel’s Secret/README.d
@@ -0,0 +1,17 @@
+Description
+
+  We found this zip archive file on a cyber squirrel’s computer but something about it seems strange, almost otherworldly. Can you figure out what this file used to be and extract the flag?
+
+
+Solution
+1. Unzip the the file and in the MainDoc file I see that a graphic application was used 'Krita'
+2. Downloaded the application and imported each layer on the picture to display the flag
+    
+
+    
+
+Flag
+
+
+    
+  
